1f544fecec151f4b3c50482cbf0ebb8da156bd37
gabri <gabri@DESKTOP-ML3L5O0>
2022-05-15 15:08:40 -0300

    Builder Veiculo - Pronto

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -11,37 +11,9 @@ public class Principal {
 
         Veiculo caminhao = VeiculoBuilder.builder()
                 .addVeiculo(
-                        TipoVeiculo.MOTOCICLETA,
-                        "XXX",
-                        "ABC",
-                        "ABCD65",
-                        "",
-                        160)
+                        TipoVeiculo.MOTOCICLETA,"XXX","ABC","ABCD65","",160)
                 .addCilindradas(200)
                 .get();
-
-        // Veiculo moto = VeiculoBuilder.builder()
-        // .addVeiculo(
-        // TipoVeiculo.MOTOCICLETA,
-        // "GAT1515",
-        // "HONDA",
-        // "CG150",
-        // "Vermelha",
-        // 130)
-        // .addCilindradas(150)
-        // .get();
-
-        // Veiculo van = VeiculoBuilder.builder()
-        // .addVeiculo(
-        // TipoVeiculo.VAN,
-        // "EMA1012",
-        // "Mercedes Benz",
-        // "Sprinter",
-        // "Branca",
-        // 140)
-        // .addPassageiros(12)
-        // .addParadas(12)
-        // .get();
 
         mov.inicioMovimentacao(caminhao);
     }
